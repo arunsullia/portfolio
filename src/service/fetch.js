42dcf5a49b122b5f29c2ea5d9802fc237e397f95
arunsullia <arunmb45@gmail.com>
2021-12-31 17:40:09 +0530

    commented fbconfig

--- src/service/fetch.js
+++ src/service/fetch.js
@@ -1,15 +1,19 @@
-import db from "../config/fbconfig";
+// import db from "../config/fbconfig";
 
 export const sendQuery = (data) => {
-    return new Promise((resolve,reject)=>{
-        db.collection("todos")
-        .add({
-          name: newTodo,
-          pending: true,
-          createAt: new Date(),
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    })
-}
+  return new Promise((resolve, reject) => {
+    resolve({
+      status: 200,
+      data: [],
+    });
+    // db.collection("todos")
+    // .add({
+    //   name: newTodo,
+    //   pending: true,
+    //   createAt: new Date(),
+    // })
+    // .catch((err) => {
+    //   reject(err);
+    // });
+  });
+};
